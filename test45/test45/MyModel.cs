b8df14c2fbcbe1c8df2d1d20bf631152f6913398
Pamela Rios <prios@nova.edu>
2020-02-19 23:35:57 -0500

    Add first sample

---
+++ test45/test45/MyModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace test45
+{
+    public class MyModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Num
+        {
+            get;
+            set;
+        }
+
+        private string imagePath = "plusign.png";
+        public string ColorImage
+        {
+            get { return imagePath; }
+            set
+            {
+                imagePath = value;
+                NotifyPropertyChanged("ColorImage");
+            }
+        }
+
+
+        public MyModel()
+        {
+        }
+    }
+}
---
+++ test45/test45/ViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace test45
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand command { get; set; }
+
+        public ObservableCollection<MyModel> MyItemsSource { get; set; }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void AddImg(object obj)
+        {
+            var model = obj as MyModel;
+
+            model.ColorImage = "minusign.png";
+
+        }
+
+
+        public ViewModel()
+        {
+            MyItemsSource = new ObservableCollection<MyModel>() {
+    new MyModel()
+        {
+            Name = "Umair", Num = "0456445450945",
+        },
+        new MyModel()
+        {
+            Name = "Cat", Num = "034456445905",
+        },
+        new MyModel()
+        {
+            Name = "Nature", Num = "56445905",
+        },
+};
+
+
+    
+
+
+
+            command = new Command(AddImg);
+
+        }
+
+    
+
+
+}
+    }
